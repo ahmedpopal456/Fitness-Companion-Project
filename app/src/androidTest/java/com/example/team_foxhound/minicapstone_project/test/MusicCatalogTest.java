755ba4d4dad6d4f32758ce1517d8fa64c70bd551
philsylvestreca <philsylvestreca@gmail.com>
2015-11-11 13:42:51 -0500

    added tests

--- app/src/androidTest/java/com/example/team_foxhound/minicapstone_project/test/MusicCatalogTest.java
+++ app/src/androidTest/java/com/example/team_foxhound/minicapstone_project/test/MusicCatalogTest.java
@@ -1,6 +1,10 @@
 package com.example.team_foxhound.minicapstone_project.test;
 
+import com.example.team_foxhound.minicapstone_project.Interfaces.MusicCatalogInterface;
+
 import junit.framework.TestCase;
+
+import org.junit.Test;
 
 /**
  * Created by Nitesh on 01-Nov-15.
@@ -15,4 +19,23 @@ public class MusicCatalogTest extends TestCase {
     public void tearDown() throws Exception {
 
     }
+
+    @Test
+    public void testMusicCatalog() {
+        MusicCatalogInterface i = new MusicCatalog();
+        assertEquals(i.getPath(), "path");
+    }
+
+    @Test
+    public void testgetBbm() {
+        MusicCatalogInterface i = new MusicCatalog();
+    }
+    }
+    public int getBbm() {
+
+    }
+
+    @Override
+    public String getName() {
+        return null
 }
